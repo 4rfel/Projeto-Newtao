6ea7a480b8786ad98953f977b9e69e325a281910
Henrique Thome <henrique.thome@hotmail.com>
2018-05-07 12:03:01 -0300

    classes

---
+++ Apple.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Apple(pygame.sprite.Sprite):
+    def __init__(self, imagem,x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(imagem)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
