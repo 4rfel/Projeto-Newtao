6798e9ecc936b7cfc9f6738f78d41c56c2afde41
4rfel <38050598+4rfel@users.noreply.github.com>
2018-05-11 08:46:28 -0300

    Delete Heart.py

--- Heart.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Heart(pygame.sprite.Sprite):
-    def __init__(self, imagem, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(imagem)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
